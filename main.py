fe7ccd9981b4eefcb08fb52d26d9726c4ef4d56c
Andrew P <pyea0230@gmail.com>
2026-05-17 14:57:49 +1000

    Added electricity usage data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import geopandas as geo_pd
 
 power_stations_df = pd.read_csv("data/power_stations.csv")
 
@@ -9,3 +10,6 @@ transmission_lines_df = pd.read_csv("data/power_stations.csv")
 
 transmission_lines_df.index = transmission_lines_df["objectid"]
 transmission_lines_df.drop("objectid", axis=1, inplace=True)
+
+x = geo_pd.read_file("data/ASGS_2021_MAIN_STRUCTURE/ASGS_2021_Main_Structure_GDA2020.gpkg")
+print(x.head())
