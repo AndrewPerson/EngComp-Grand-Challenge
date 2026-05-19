1a063842ffcf8c3330c3bbcce0fbf73ebaffd348
Andrew P <pyea0230@gmail.com>
2026-05-19 17:09:47 +1000

    Partial progress on renewable energy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import geopandas as geo_pd
 
+import renewable_generation_vs_sources
+
 power_stations_df = pd.read_csv("data/power_stations.csv")
 
 power_stations_df.index = power_stations_df["objectid"]
@@ -8,6 +10,9 @@ power_stations_df.drop("objectid", axis=1, inplace=True)
 
 power_stations_df["geometry"] = geo_pd.points_from_xy(power_stations_df["x_coordinate"], power_stations_df["y_coordinate"])
 power_stations_df.drop(["x_coordinate", "y_coordinate"], axis=1, inplace=True)
+
+power_stations_df["operationalstatus"] = power_stations_df["operationalstatus"].astype("category")
+power_stations_df["primaryfueltype"] = power_stations_df["primaryfueltype"].astype("category")
 
 power_stations_df = geo_pd.GeoDataFrame(power_stations_df)
 
@@ -33,12 +38,14 @@ qld_electricity_consumption.drop("Local Government Area", axis=1, inplace=True)
 # Example plot
 import matplotlib.pyplot as plt
 
-lga_df["geometry"].boundary.plot()
-plt.scatter(
-    power_stations_df["x_coordinate"],
-    power_stations_df["y_coordinate"],
-    s=power_stations_df["generationmw"] / 3 + 1
-)
+# lga_df["geometry"].boundary.plot()
+# plt.scatter(
+#     power_stations_df["geometry"].x,
+#     power_stations_df["geometry"].y,
+#     s=power_stations_df["generationmw"] / 10 + 5
+# )
+
+renewable_generation_vs_sources.plot(plt.gca(), power_stations_df, lga_df)
 
 plt.axis("equal")
 plt.show()
--- renewable_generation_vs_sources.py
+++ renewable_generation_vs_sources.py
@@ -1,6 +1,21 @@
 from matplotlib.pylab import Axes
+import matplotlib as mpl
 import pandas as pd
+import geopandas as geo_pd
 
 
-def plot(axes: Axes, example_df: pd.DataFrame):
-    ...
+def plot(axes: Axes, power_stations_df: geo_pd.GeoDataFrame, lga_df: geo_pd.GeoDataFrame):
+    lga_df.boundary.plot(ax=axes)
+
+    renewable_power_stations_df = power_stations_df[power_stations_df["primaryfueltype"].isin(["Water", "Wind", "Solar", "Biogas", "Biomass"])]
+    renewable_power_stations_df = renewable_power_stations_df[renewable_power_stations_df["operationalstatus"] == "Operational"]
+
+    greens = mpl.colormaps["viridis"]
+
+    print(pd.factorize(renewable_power_stations_df["primaryfueltype"])[0] / (len(renewable_power_stations_df["primaryfueltype"].cat.categories) - 1))
+
+    axes.scatter(
+        renewable_power_stations_df["geometry"].x,
+        renewable_power_stations_df["geometry"].y,
+        c=greens(pd.factorize(renewable_power_stations_df["primaryfueltype"])[0] / (len(renewable_power_stations_df["primaryfueltype"].cat.categories) - 1))
+    )
